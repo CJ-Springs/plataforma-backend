1059d9e00ab7a90a724ca675b040624e879eeb08
FrancoMusolino <francomusolino55@gmail.com>
2023-07-19 14:18:15 -0300

    (sales)[CSP-16] event flow on sale order placed (decrement stock & register movement)

--- src/business/warehouse/springs/events/handlers/index.ts
+++ src/business/warehouse/springs/events/handlers/index.ts
@@ -3,11 +3,13 @@ import { IncomeOrderConfirmedHandler } from './income-order-confirmed.handler'
 import { StockIncrementedHandler } from './stock-incremented.handler'
 import { WarrantyOrderCreatedHandler } from './warranty-order-created.handler'
 import { StockDecrementedHandler } from './stock-decremented.handler'
+import { SaleOrderPlacedHandler } from './sale-order-placed.handler'
 
 export const EventHandlers = [
   StockAdjustedHandler,
   StockIncrementedHandler,
   StockDecrementedHandler,
   IncomeOrderConfirmedHandler,
+  SaleOrderPlacedHandler,
   WarrantyOrderCreatedHandler,
 ]
